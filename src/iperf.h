1ac3746379d07e6cfa8ec95f98cacac3f1f0a94c
Jakub Kicinski <kuba@kernel.org>
2022-02-02 09:38:24 -0800

    Report number of reorder_seen
    TCP can detect reordering by tracking SACKs and timestamps
    coming back to the sender. This is useful information for
    investigating performance issues and testing networks as
    good networks should generally not reorder packets.

--- src/iperf.h
+++ src/iperf.h
@@ -109,6 +109,7 @@ struct iperf_interval_results
     long rtt;
     long rttvar;
     long pmtu;
+    long reorder;
 };
 
 struct iperf_stream_result
@@ -120,6 +121,7 @@ struct iperf_stream_result
     iperf_size_t bytes_sent_omit;
     long stream_prev_total_retrans;
     long stream_retrans;
+    long stream_reorder;
     long stream_max_rtt;
     long stream_min_rtt;
     long stream_sum_rtt;
